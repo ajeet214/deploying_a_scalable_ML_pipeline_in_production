300bfc6650d4beb5fc594b17e2804271f686a301
ajeet <ajeet214.90@gmail.com>
2023-11-25 23:05:35 +0700

    add comments and doc string

--- train.py
+++ train.py
@@ -1,3 +1,19 @@
+"""
+train.py
+
+This script serves as the entry point for training the machine learning model. It uses Hydra for configuration management,
+loading the configuration from the specified path and triggering the training process by calling the 'training' function
+from the 'train_model' module.
+
+Usage:
+- Run this script to initiate the model training with the provided configuration.
+
+Note:
+- Make sure Hydra is installed to support configuration management.
+
+Author:
+Ajeet Kumar Verma
+"""
 import hydra
 from module.train_model import training
 from omegaconf import DictConfig
@@ -5,8 +21,12 @@ from omegaconf import DictConfig
 
 @hydra.main(config_path=".", config_name="config", version_base="1.2")
 def main(config: DictConfig):
+    # Entry point of the script using Hydra for configuration management.
+    # It loads the configuration from the specified path and runs the training function.
+
     training(config)
 
 
 if __name__ == "__main__":
+    # Execute the main function when the script is run directly.
     main()
